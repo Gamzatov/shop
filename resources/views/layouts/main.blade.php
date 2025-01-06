6905e2c2bb6e3973bd5911571316c60d6ec3de30
Artem Dirksen <dirksen.tema@gmail.com>
2025-01-06 18:01:41 +0200

    Fix merge conflicts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -213,7 +213,7 @@
             <b>Version</b> 3.2.0
         </div>
     </footer>
-    <!-- Control Sidebar -->
+
     <aside class="control-sidebar control-sidebar-dark">
         <!-- Control sidebar content goes here -->
     </aside>
